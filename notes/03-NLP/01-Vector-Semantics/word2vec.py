0383771d4b73c1d3439a481ed89b925d8873185e
cbqin <cbqin0323@gmail.com>
2021-02-05 17:10:56 +0800

    update word2vec

--- notes/03-NLP/01-Vector-Semantics/word2vec.py
+++ notes/03-NLP/01-Vector-Semantics/word2vec.py
@@ -12,6 +12,36 @@ def normalizeRows(x):
     return x
 
 
+def softmax(x):
+    """Compute the softmax function for each row of the input x.
+    It is crucial that this function is optimized for speed because
+    it will be used frequently in later code. 
+    Arguments:
+    x -- A D dimensional vector or N x D dimensional numpy matrix.
+    Return:
+    x -- You are allowed to modify x in-place
+    """
+    orig_shape = x.shape
+
+    if len(x.shape) > 1:
+        # matrix
+        tmp = np.max(x, axis=1)
+        x -= tmp.reshape((x.shape[0], 1))
+        x = np.exp(x)
+        tmp = np.sum(x, axis=1)
+        x /= tmp.reshape((x.shape[0], 1))
+    else:
+        # vector
+        tmp = np.max(x)
+        x -= tmp
+        x = np.exp(x)
+        tmp = np.sum(x)
+        x /= tmp
+
+    assert x.shape == orig_shape
+    return x
+
+
 def sigmoid(x):
     """
     Compute the sigmoid function for the input here.
