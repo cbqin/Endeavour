e237d48e195c260365a8c4117c2ea80d86d3d4aa
cbqin <cbqin0323@gmail.com>
2021-02-24 17:38:52 +0800

    update glove

---
+++ notes/03-NLP/01-Vector-Semantics/GloVe/src/glove.py
@@ -0,0 +1,12 @@
+import numpy as np
+import random
+
+from scipy import sparse
+
+
+def glove():
+    pass
+
+
+def glove_adagrad_wrapper():
+    pass
---
+++ notes/03-NLP/01-Vector-Semantics/GloVe/src/utils.py
@@ -0,0 +1,21 @@
+from functools import wraps
+
+
+def listify(fn):
+    """
+    Use this decorator on a generator function to make it return a list
+    instead.
+    """
+    @wraps(fn)
+    def listified(*args, **kwargs):
+        return list(fn(*args, **kwargs))
+
+    return listified
+
+
+def build_vocab(corpus):
+    pass
+
+
+def build_cooccur(vocab, corpus):
+    pass
