c1c42f651351dc26dd201c54a50c93ad516ad22b
cbqin <cbqin0323@gmail.com>
2021-02-26 14:44:33 +0800

    add glove

--- notes/03-NLP/01-Vector-Semantics/GloVe/src/glove.py
+++ notes/03-NLP/01-Vector-Semantics/GloVe/src/glove.py
@@ -4,7 +4,32 @@ import random
 from scipy import sparse
 
 
-def glove():
+def lossAndGradient(
+    centerWordVec,
+    centerBias,
+    contextWordVec,
+    contextBias,
+    cooccurrence,
+    xMax=100,
+    alpha=0.75
+):
+    weight = (cooccurrence / xMax)**alpha if cooccurrence < xMax else 1
+
+    innerLoss = np.dot(centerWordVec, contextWordVec) + \
+        centerBias + contextBias + np.log(cooccurrence)
+    loss = weight * innerLoss**2
+
+    gradCenterVec = weight * innerLoss * contextWordVec
+    gradContextVec = weight * innerLoss * centerWordVec
+
+    gradCenterBias = weight * innerLoss
+    gradContextBias = weight * innerLoss
+
+    return loss, gradCenterVec, gradContextVec, gradCenterBias, gradContextBias
+
+
+def glove(currentCenterWord, word2Ind,
+          centerWordVectors, outsideVectors):
     pass
 
 
